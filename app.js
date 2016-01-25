4fb8589666af8c40d130703bbb4dc26d0eaaf4c7
codyss <cody.schwarz@gmail.com>
2016-01-25 17:21:21 -0500

    set up file structure, app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var routes = require('./routes/');
+var morgan = require('morgan');
+var bodyParser = require('body-parse');
+var swig = require('swig');
+var path = require('path');
+var fs = require('fs');
+
+//set up swing
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
+
+//log the requets
+app.use(morgan('dev'));
+
+//static routing for public file
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Read in for post requests
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json());
+
+//All routes go to routes
+app.use('/', routes);
+
+
+app.listen(port);
+console.log('Magic happening on: ', port);
